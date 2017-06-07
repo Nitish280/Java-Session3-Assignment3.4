8df2ff160d7caa0ec60f50c8282c3344e09d2474
Nitish280 <nsrccs@gmail.com>
2017-06-07 19:49:09 +0530

    Update Typecasting.java

--- src/Typecasting.java
+++ src/Typecasting.java
@@ -4,27 +4,31 @@ public class Typecasting {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//we uses scanner here because we have tto take the input from the user
 		Scanner in  = new Scanner(System.in);
-		
+		//here we take the first input from the user
 		System.out.println("Input first number: ");  
 	    byte num1 = in.nextByte();
-	    
-	    System.out.println("Input first number: ");  
+	    //here we take the first input from the user
+	    System.out.println("Input Second number: ");  
 	    short num2 = in.nextShort();
-	    
+	    //we can take initialy byte and short type of variable
 	    byte x = num1;
 	    short y = num2;
 	    int z=0;
 	    long u=0;
 	    float v=0;
 	    double d=0;
-	    
-	    z=x+y;
+            //here we are doing typecastig we put value of byte and short in int
+	     z=x+y;
 	    System.out.println("Sum of byte and short:" +z);
+		//here we put value of short and int in long
 	    u=y+z;
 	    System.out.println("Sum of short and int:" +u);
+		//here we put value of int and long in float
 	    v=z+u;
 	    System.out.println("Sum of int and long:" +v);
+		//here we put value of long and float in double
 	    d=v+u;
 	    System.out.println("Sum of float and double:" +d);
 	    
